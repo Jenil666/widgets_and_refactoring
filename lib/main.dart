74aa44daca68a0f75c55f20517c416737da80646
Jenil666 <jenilchandarana33@gmail.com>
2023-01-18 17:20:30 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:widgets_and_refactoring/6.1.3.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/':(context)=>Project3(),
+      },
+    ),
+  );
+}
